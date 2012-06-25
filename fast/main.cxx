33f7b9cc53ea9141da9b4caeb98a4e3f8dee188c
Rio Yokota <rioyokota@users.noreply.github.com>
2012-06-25 13:32:04 -0400

    Bugfix for GPU version. types.h set for GPU runs.

---
+++ fast/main.cxx
@@ -0,0 +1,96 @@
+/*
+Copyright (C) 2011 by Rio Yokota, Simon Layton, Lorena Barba
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include "dataset.h"
+#include "tree.h"
+
+int main(int argc, char** argv) {
+  int numBodies = 1000;
+  IMAGES = 0;
+  THETA = 0.6;
+  Bodies bodies, bodies2;
+  Cells cells;
+  Dataset DATA;
+  SerialFMM FMM;
+  FMM.NDLEAF_THRESHOLD = argc > 1 ? atoi(argv[1]) : 10000;
+#if HYBRID
+  FMM.timeKernels();
+#endif
+#ifdef MANY
+  for ( int it=0; it<25; it++ ) {
+  numBodies = int(pow(10,(it+24)/8.0));
+#else
+  for ( int it=0; it<1; it++ ) {
+  FMM.printNow = true;
+#if BUILD
+  numBodies = 10000000;
+#else
+  numBodies = 1000000;
+#endif
+#endif
+  std::cout << "N                    : " << numBodies << std::endl;
+  bodies.resize(numBodies);
+  DATA.cube(bodies);
+  FMM.startTimer("FMM");
+#if BOTTOMUP
+  FMM.bottomup(bodies,cells);
+#else
+  FMM.topdown(bodies,cells);
+#endif
+#if BUILD
+#else
+  FMM.startPAPI();
+#if IneJ
+  FMM.evaluate(cells,cells);
+#else
+  FMM.evaluate(cells);
+#endif
+  FMM.stopPAPI();
+  FMM.stopTimer("FMM",true);
+  FMM.eraseTimer("FMM");
+  FMM.writeTime();
+  FMM.resetTimer();
+
+  bodies2 = bodies;
+//#ifdef MANY
+  if (bodies2.size() > 100) bodies2.resize(100);
+//#endif
+  DATA.initTarget(bodies2);
+  FMM.startTimer("Direct sum");
+//#if IneJ
+  FMM.direct(bodies2,bodies);
+//#elif MANY
+//  FMM.direct(bodies2,bodies);
+//#else
+//  FMM.direct(bodies2);
+//#endif
+  FMM.stopTimer("Direct sum",true);
+  FMM.eraseTimer("Direct sum");
+
+//#ifdef MANY
+  if (bodies.size() > 100) bodies.resize(100);
+//#endif
+  real diff1 = 0, norm1 = 0, diff2 = 0, norm2 = 0;
+  DATA.evalError(bodies,bodies2,diff1,norm1,diff2,norm2);
+  DATA.printError(diff1,norm1,diff2,norm2);
+#endif
+  }
+}
---
+++ fast/topdown.h
@@ -0,0 +1,248 @@
+/*
+Copyright (C) 2011 by Rio Yokota, Simon Layton, Lorena Barba
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#ifndef topdown_h
+#define topdown_h
+#include "evaluator.h"
+
+class TopDown : public Evaluator {
+private:
+  Nodes    nodes;
+  Leafs    leafs;
+  B_iter   BN;
+  C_iter   CN;
+  unsigned NLEAF;
+  unsigned NCELL;
+
+protected:
+  int      MAXLEVEL;
+
+private:
+  void init(Node &node) {
+    node.NOCHILD = true;
+    node.NLEAF = 0;
+    node.LEAF = NULL;
+    for( int b=0; b!=8; ++b ) node.CHILD[b] = -1;
+  }
+
+  inline void addChild(int octant, bigint n) {
+    N_iter N = nodes.begin()+n;
+    Node child;
+    init(child);
+    child.LEVEL = N->LEVEL+1;
+    child.X = N->X;
+    real r = R0 / (1 << child.LEVEL);
+    for( int d=0; d!=3; ++d ) {                                 // Loop over dimensions
+      child.X[d] += r * (((octant & 1 << d) >> d) * 2 - 1);     //  Calculate new center position
+    }                                                           // End loop over dimensions
+    N->NOCHILD = false;
+    N->CHILD[octant] = nodes.size();
+    nodes.push_back(child);
+    NCELL++;
+  }
+
+  void splitNode(bigint n) {
+    while( nodes[n].NLEAF > NCRIT ) {
+      int c = 0;
+      Leaf *Ln;
+      for( Leaf *L=nodes[n].LEAF; L; L=Ln ) {
+	N_iter N = nodes.begin()+n;
+        Ln = L->NEXT;
+        int octant = (L->X[0] > N->X[0]) + ((L->X[1] > N->X[1]) << 1) + ((L->X[2] > N->X[2]) << 2);
+        if( N->CHILD[octant] == -1 ) {
+          addChild(octant,n);
+        }
+        c = nodes[n].CHILD[octant];
+        Node *child = &nodes[c];
+        L->NEXT = child->LEAF;
+        child->LEAF = &*L;
+        child->NLEAF++;
+      }
+      n = c;
+    }
+  }
+
+  void nodes2cells(int i, C_iter C) {
+    C->R      = R0 / (1 << nodes[i].LEVEL);
+    C->X      = nodes[i].X;
+    C->NDLEAF = nodes[i].NLEAF;
+    C->LEAF   = BN;
+    if( nodes[i].NOCHILD ) {
+      C->CHILD = 0;
+      C->NCHILD = 0;
+      C->NCLEAF = nodes[i].NLEAF;
+      for( Leaf *L=nodes[i].LEAF; L; L=L->NEXT ) {
+        BN->IBODY = L->I;
+        BN++;
+      }
+    } else {
+      C->NCLEAF = 0;
+      int nsub=0;
+      for( int octant=0; octant!=8; ++octant ) {
+        if( nodes[i].CHILD[octant] != -1 ) {
+          ++nsub;
+        }
+      }
+      C_iter Ci = CN;
+      C->CHILD = Ci - Ci0;
+      C->NCHILD = nsub;
+      CN += nsub;
+      for( int octant=0; octant!=8; ++octant ) {
+        if( nodes[i].CHILD[octant] != -1 ) {
+          Ci->PARENT = C - Ci0;
+          nodes2cells(nodes[i].CHILD[octant], Ci++);
+        }
+      }
+    }
+  }
+
+  void permuteBodies(Bodies &bodies) {
+    Bodies buffer = bodies;
+    for( B_iter B=bodies.begin(); B!=bodies.end(); ++B ) {      // Loop over bodies
+      B->ICELL = buffer[B->IBODY].ICELL;
+      B->X     = buffer[B->IBODY].X;
+      B->SRC   = buffer[B->IBODY].SRC;
+      B->TRG   = buffer[B->IBODY].TRG;
+    }
+  }
+
+protected:
+  void setDomain(Bodies &bodies) {
+    startTimer("Set domain");
+    vect xmin, xmax;
+    NLEAF = bodies.size();
+    leafs.reserve(NLEAF);
+    X0 = 0;
+    xmax = xmin = bodies.begin()->X;
+    for( B_iter B=bodies.begin(); B!=bodies.end(); ++B ) {
+      Leaf leaf;
+      leaf.I = B-bodies.begin();
+      leaf.X = B->X;
+      leafs.push_back(leaf);
+      for( int d=0; d!=3; ++d ) {
+        if     (B->X[d] < xmin[d]) xmin[d] = B->X[d];
+        else if(B->X[d] > xmax[d]) xmax[d] = B->X[d];
+      }
+      X0 += B->X;
+    }
+    X0 /= bodies.size();
+    for( int d=0; d!=3; ++d ) {
+      X0[d] = int(X0[d]+.5);
+      R0 = std::max(xmax[d] - X0[d], R0);
+      R0 = std::max(X0[d] - xmin[d], R0);
+    }
+    R0 *= 1.000001;
+    stopTimer("Set domain",printNow);
+  }
+
+  void buildTree() {
+    startTimer("Grow tree");
+    NCELL = 1;
+    nodes.reserve(NLEAF);
+    Node node;
+    init(node);
+    node.LEVEL = 0;
+    node.X     = X0;
+    nodes.push_back(node);
+    MAXLEVEL = 0;
+    for( L_iter L=leafs.begin(); L!=leafs.end(); ++L ) {
+      /* we use node index rather than iterator
+	 since nodes may expand along the way */
+      int n = 0;
+      while( !nodes[n].NOCHILD ) {
+	N_iter N = nodes.begin() + n;
+        int octant = (L->X[0] > N->X[0]) + ((L->X[1] > N->X[1]) << 1) + ((L->X[2] > N->X[2]) << 2);
+        N->NLEAF++;
+        if( nodes[n].CHILD[octant] == -1 ) addChild(octant,n);
+        n = nodes[n].CHILD[octant];
+      }
+      L->NEXT = nodes[n].LEAF;
+      nodes[n].LEAF = &*L;
+      nodes[n].NLEAF++;
+      if( nodes[n].NLEAF > NCRIT ) splitNode(n);
+      if( MAXLEVEL < nodes[n].LEVEL ) MAXLEVEL = nodes[n].LEVEL;
+    }
+    MAXLEVEL++;
+    stopTimer("Grow tree",printNow);
+  }
+
+  void linkTree(Bodies &bodies, Cells &cells) {
+    startTimer("Link tree");
+    cells.resize(NCELL);
+    Ci0 = cells.begin();
+    BN = bodies.begin();
+    CN = Ci0 + 1;
+    nodes2cells(0,Ci0);
+    nodes.clear();
+    leafs.clear();
+    permuteBodies(bodies);
+    stopTimer("Link tree",printNow);
+  }
+
+  void upwardPass(Cells &cells) {
+    startTimer("Upward pass");
+    setRootCell(cells);
+    for( C_iter C=cells.begin(); C!=cells.end(); ++C ) {
+      C->M = 0;
+      C->L = 0;
+    }
+    for( C_iter C=cells.end()-1; C!=cells.begin()-1; --C ) {
+      real Rmax = 0;
+      setCenter(C);
+      P2M(C,Rmax);
+      M2M(C,Rmax);
+    }
+#if Cartesian
+    for( C_iter C=cells.begin(); C!=cells.end(); ++C ) {
+      for( int i=1; i<MTERM; ++i ) C->M[i] /= C->M[0];
+    }
+#endif
+    setRcrit(cells);
+    stopTimer("Upward pass",printNow);
+  }
+
+  void downwardPass(Cells &cells) const {
+    C_iter C0 = cells.begin();
+    L2P(C0);
+    for( C_iter C=C0+1; C!=cells.end(); ++C ) {
+      L2L(C);
+      L2P(C);
+    }
+  }
+
+  void printTreeData(Cells &cells) {
+    setRootCell(cells);
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << "Root center          : " << ROOT->X              << std::endl;
+    std::cout << "Root radius          : " << R0                   << std::endl;
+    std::cout << "Bodies               : " << ROOT->NDLEAF         << std::endl;
+    std::cout << "Cells                : " << cells.size()         << std::endl;
+    std::cout << "Tree depth           : " << MAXLEVEL             << std::endl;
+    std::cout << "Total charge         : " << std::abs(ROOT->M[0]) << std::endl;
+    std::cout << "P2P calls            : " << NP2P                 << std::endl;
+    std::cout << "M2P calls            : " << NM2P                 << std::endl;
+    std::cout << "M2L calls            : " << NM2L                 << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+  }
+
+};
+
+#endif
